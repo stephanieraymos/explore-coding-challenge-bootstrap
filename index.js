36e78106bbcc039b63f688c831dbb453a280ce7a
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-03 11:55:30 -0800

    Modal functionality + modal styling

---
+++ index.js
@@ -0,0 +1,20 @@
+//carousel pause and play
+$(function() {
+
+//moreDetails button modal show
+$("#button").click(function(){
+    $("#moreDetails").modal("show");
+    // if ($(".close").modal("hide"));
+ //moreDetails button modal hide
+  });
+  $(".close").click(function(){
+    $("#moreDetails").modal("hide");
+
+  });
+
+//more details button modal hide
+    $(".close").click(function(){
+        $("#moreDetails").modal("hide");
+    
+      });
+  });
